222b97a5aa542b001134b462a9dd78a212118314
GoldenGrape <goldengrape@goldengrape.org>
2019-08-14 17:11:12 +0800

    Initial Commit

---
+++ GO_board/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GO_board
+//
+//  Created by GoldenGrape on 2019/8/14.
+//  Copyright © 2019 GoldenGrape. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
